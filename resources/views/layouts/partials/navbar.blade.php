c6e451b50b7115789fcd15de2f05e8e5c4593419
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2022-01-08 03:26:26 +0000

    Update navbar.blade.php

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar top-navbar navbar-expand-md navbar-dark">
         <div class="navbar-header" data-logobg="skin6">
             <a class="navbar-brand" style="padding: 0 10px 0 20px !important;" href="{{ route('painel.index') }}">
-                <span style="padding-top:8px"> <img src="/assets/images/logo-color.png" style="width:100%"> </span>
+                <span style="padding-top:8px"> <img src="/assets/images/logo-color.png" style="width:95%;left:0px;"> </span>
             </a>
             <a class="nav-toggler waves-effect waves-light text-dark d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
         </div>
