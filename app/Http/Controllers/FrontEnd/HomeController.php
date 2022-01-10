3b852586c83640664e6102b9059a6e827b4647f7
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2022-01-10 22:22:32 +0044

    Update HomeController.php

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc')->get();
 
         return view('frontend.index')->with('posts', $posts);
     }
